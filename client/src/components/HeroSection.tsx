f15f874c8ac6358495b00aa5fdddd84ce641aa01
AdhirathRM <adhirathrajeshmenonbtech24@rvu.edu.in>
2026-05-03 00:55:20 +0530

    hopefully the end

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -34,7 +34,7 @@ export const HeroSection = () => {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: 'url(/ensamble.png)',
+          backgroundImage: 'url(/oath.png)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundAttachment: 'fixed',
